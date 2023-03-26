4c21839feaf857269d59a6a7a2b6ef2374a4491a
malken21 <Malken_Marumasa@outlook.jp>
2023-03-27 02:49:02 +0900

    setup

---
+++ src/main/java/marumasa/track/minecraft.java
@@ -0,0 +1,17 @@
+package marumasa.track;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class minecraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
